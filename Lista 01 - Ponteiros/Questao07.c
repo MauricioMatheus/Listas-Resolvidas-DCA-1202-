2f5f4dc1a6773240c4114dd8eade6d3e2713b08b
Mauricio <mauriciomatheuss2001@gmail.com>
2023-04-28 22:21:59 -0300

    Lista de Ponteiros

---
+++ Lista 01 - Ponteiros/Questao07.c
@@ -0,0 +1,10 @@
+/*Considerando a declaração int mat[4], *p, x;, quais das seguintes expressões são válidas?
+Justifique. */
+
+p = mat + 1; // Válido. p está sendo definido como um ponteiro para mat + 1, ou mat[5];
+
+p = mat++; /* Inválido. Não é possivel fazer incrementação de tipo int ao ponteiro; */
+
+p = ++mat; // Notação inválida.
+
+x = (*mat)++; // Válido. O resultado seria o endereço em mat[0] incrementado em 1.
